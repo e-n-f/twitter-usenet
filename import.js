db8c38ee99c9daeea82f5d9ac6d9cbe77264b46f
Eric Fischer <enf@pobox.com>
2017-11-13 22:28:56 -0800

    Pull apart JSON

--- import.js
+++ import.js
@@ -1,8 +1,8 @@
 #!/usr/local/bin/node
 
-'use strict';
+"use strict";
 
-let unixio = require('unixio');
+let unixio = require("unixio");
 
 async function read_active() {
 	let ret = {};
@@ -18,9 +18,49 @@ async function read_active() {
 	return ret;
 }
 
+async function convert(text, active) {
+	if ("timestamp_ms" in text) {
+		if ("extended_tweet" in text) {
+			text.text = text.extended_tweet.full_text;
+		}
+		await unixio.stdout.puts(JSON.stringify(text) + "\n");
+	}
+}
+
 async function main() {
 	let active = await read_active();
-	unixio.stdout.puts(JSON.stringify(active));
+
+	let s;
+	while ((s = await unixio.stdin.gets()) != null) {
+		if (s.startsWith("[")) {
+			let j;
+
+			try {
+				j = JSON.parse(s);
+			} catch (e) {
+				await unixio.stderr.puts(s.substring(0, 200) + ": ");
+				await unixio.stderr.puts(e.toString());
+				continue;
+			}
+
+			let i;
+			for (i = 0; i < j.length; j++) {
+				await convert(j[i], active);
+			}
+		} else if (s !== "\n" && s != "\r\n") {
+			let j;
+
+			try {
+				j = JSON.parse(s);
+			} catch (e) {
+				await unixio.stderr.puts(s.substring(0, 200) + ": ");
+				await unixio.stderr.puts(e.toString());
+				continue;
+			}
+
+			await convert(j, active);
+		}
+	}
 }
 
 unixio.call(main);
